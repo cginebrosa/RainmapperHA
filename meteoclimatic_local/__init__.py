e94e3ffba57267cba242e8b75324d145992c468f
cginebrosa <46960519+cginebrosa@users.noreply.github.com>
2024-02-14 10:14:55 +0100

    last run

--- meteoclimatic_local/__init__.py
+++
@@ -1,4 +0,0 @@
-from meteoclimatic_local.weather import Weather, Condition  # noqa: F401
-from meteoclimatic_local.station import Station  # noqa: F401
-from meteoclimatic_local.observation import Observation  # noqa: F401
-from meteoclimatic_local.client import MeteoclimaticClient  # noqa: F401
